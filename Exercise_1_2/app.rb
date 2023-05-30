6cd1b9ea72d8fe1e989c778d084f4a1bc92156a6
Della Izadi <delaram.ae@gmail.com>
2023-05-30 15:30:25 +0100

    First exercise

---
+++ Exercise_1_2/app.rb
@@ -0,0 +1,11 @@
+#This is the main file of the applicatioin and contain application class
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+  end
+end
