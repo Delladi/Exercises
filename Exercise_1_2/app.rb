496530b7204ee08bf8b62064e5df2f6b54765742
Della Izadi <delaram.ae@gmail.com>
2023-05-31 14:53:22 +0100

    Get to names Exercise 3 app file completed

--- Exercise_1_2/app.rb
+++ Exercise_1_2/app.rb
@@ -3,16 +3,22 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Application < Sinatra::Base
-  post '/submit' do
-    name = params[:name]
-    message = params[:message]
-
-    return "Thanks #{name}, you sent this message: #{message}"
+  get '/names' do
+    names = params[:names]
+    return "#{names}"
   end
-
+  
   # This allows the app code to refresh
   # without having to restart the server.
 configure :development do
     register Sinatra::Reloader
   end
 end
+
+# get '/names' do
+#   names = params[:names]
+#   names = names.split(",")
+#   names = names.sort
+#   names = names.join(",")
+#   return "#{names}"
+# end
