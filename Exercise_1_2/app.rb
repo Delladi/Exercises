c9826cebef81acef98dc8c1260d207aac94ba4d8
Della Izadi <delaram.ae@gmail.com>
2023-05-31 11:25:54 +0100

    Exercise Hello Leo

--- Exercise_1_2/app.rb
+++ Exercise_1_2/app.rb
@@ -3,9 +3,15 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Application < Sinatra::Base
+  get '/hello' do
+    name = params[:name]
+
+    return "Hello #{name}"
+  end
+
   # This allows the app code to refresh
   # without having to restart the server.
-  configure :development do
+configure :development do
     register Sinatra::Reloader
   end
 end
