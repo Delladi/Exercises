3b5077da3022632cbf57739f8e92df5363f12ae4
Della Izadi <delaram.ae@gmail.com>
2023-06-01 10:12:13 +0100

    Updating my tests to include tests for sorting names

--- Exercise_1_2/spec/integration/app_spec.rb
+++ Exercise_1_2/spec/integration/app_spec.rb
@@ -18,9 +18,36 @@ describe Application do
       # and returns a response object we can test.
       # Assert the response status code and body.
       response = get("/names?names=Julia,Mary,Karim")
-
+      
       expect(response.status).to eq(200)
       expect(response.body).to eq("Julia,Mary,Karim")
     end
   end
+
+  context "POST to /sort-names" do
+    it "returns a list of names that are sorted" do
+      # Send a GET request to /
+      # and returns a response object we can test.
+      # Assert the response status code and body.
+      names = "Joe,Alice,Zoe,Julia,Kieran"
+      expected_response = "Alice,Joe,Julia,Kieran,Zoe"
+
+      post('/sort-names', names: names)
+      expect(last_response.status).to eq(200)
+      # expect(response.body).to eq(expected_response)
+      expect(last_response.body).to eq(expected_response)
+      #changed response to last_response
+    end
+  end
+  
 end
+
+# #  context "POST to /sort-names" do
+# it "returns 200 OK with the right content" do
+# # Send a POST request to /
+# # and returns a response object we can test.
+# # Assert the response status code and body.
+# response = post("/sort-names/names=UNSORTED list")
+# expect(response.status).to eq(200)
+# expect(response.body).to eq("sorted list")
+# end
